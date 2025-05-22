fb39bf21fc2159c1060e856d572bdbc185ec322d
AndreyKhodulapov <andrey.corsar1993@gmail.com>
2025-05-22 22:56:20 +0300

    elementary commands 2

--- main.py
+++ main.py
@@ -22,3 +22,21 @@ except redis.ConnectionError as e:
 # client.setex("session", 3600, "active")
 ttl = client.ttl("session")
 print(f"Left {ttl} sec")
+
+client.rpush("tasks", "Task 1", "Task 2")
+tasks = client.lrange("tasks", 0, -1)
+print([task.decode() for task in tasks])
+
+client.hset("user:1001", mapping={
+    "name": "John",
+    "age": 30,
+    "email": "kek@example.com"
+})
+
+user = client.hgetall("user:1001")
+print({k.decode(): v.decode() for k, v in user.items()})
+
+client.zadd("leaderboard", {"player1": 100, "player2": 90})
+leaders = client.zrange("leaderboard", 0, -1, withscores=True)
+for player, score in leaders:
+    print(player.decode(), score)
